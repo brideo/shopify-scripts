5fe309a2e7d24287f78e9a43c2926d78ceecd227
Nathan McBride <nathan@brideo.co.uk>
2023-08-24 19:28:18 +0000

    first commit

---
+++ shopify/elasticsearch-settings.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get store name
+    var storeName = Shopify.shop;  // Shopify global object contains the shop name
+
+   console.log(storeName);
+});
