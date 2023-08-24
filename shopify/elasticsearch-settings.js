97ba4fac5ace72908b64a23ced57d60ad973ef21
Nathan McBride <nathan@brideo.co.uk>
2023-08-25 05:52:42 +1000

    Update elasticsearch-settings.js

--- shopify/elasticsearch-settings.js
+++ shopify/elasticsearch-settings.js
@@ -1,6 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get store name
-    var storeName = Shopify.shop;  // Shopify global object contains the shop name
-
-   console.log(storeName);
-});
+alert(Shopify.shop);
