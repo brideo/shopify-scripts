a6656e296e55dcdcfcdc32bbafa3e3ff28f19691
Nathan McBride <nathan@brideo.co.uk>
2023-08-25 06:10:01 +1000

    Update elasticsearch-settings.js

--- shopify/elasticsearch-settings.js
+++ shopify/elasticsearch-settings.js
@@ -1,21 +1,23 @@
 var apiEndpoint = 'https://sticker-arts-million-mean.trycloudflare.com/api/get-settings?shop=' + Shopify.shop;
 
-try {
-    var response = await fetch(apiEndpoint, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+async function fetchSettings()
+{
+    try {
+        var response = await fetch(apiEndpoint, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
 
-    if (response.ok) {
-        var data = await response.json();
-        window.elastic = {
-            settings: data
+        if (response.ok) {
+            window.elasticConfig = await response.json();
+        } else {
+            console.error('API response was not ok:', response);
         }
-    } else {
-        console.error('API response was not ok:', response);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
     }
-} catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
 }
+
+fetchSettings();
