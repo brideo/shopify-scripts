11c8e4c51d228b95625fa59920f3ad651f19a05c
Nathan McBride <nathan@brideo.co.uk>
2023-08-29 20:10:15 +1000

    Update elasticsearch-settings.js

--- shopify/elasticsearch-settings.js
+++ shopify/elasticsearch-settings.js
@@ -1,4 +1,4 @@
-var apiEndpoint = 'https://webmasters-serious-lang-needed.trycloudflare.com/api/get-settings?shop=' + Shopify.shop;
+var apiEndpoint = 'https://people-dog-congratulations-caught.trycloudflare.com/api/get-settings?shop=' + Shopify.shop;
 
 async function fetchSettings()
 {
